d328d43e0dc1cd4568636d6140a99664c2ca0ade
eldeivis13 <davidu2028@gmail.com>
2022-10-03 16:49:14 +0200

    prueba2 y 3

--- src/Prueba3.java
+++ src/Prueba3.java
@@ -36,7 +36,7 @@ public class Prueba3 extends JFrame {
 	 */
 	public Prueba3() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 738, 666);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
