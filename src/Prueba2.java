3a1784033852eeda64c10885bf7fe63dcdaa3d51
eldeivis13 <davidu2028@gmail.com>
2022-10-04 18:31:01 +0200

    prueba2

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -295,110 +295,82 @@ public class Prueba2 extends JFrame {
 	}
 	
 	public void comprobarGanador() {
-		if(boton[0].getText().equals("X") && boton[1].getText().equals("X") && boton[2].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[1].getText().equals("O") && boton[2].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
+		if(contador >= 1) {
+			if(boton[0].getText().equals("X") && boton[1].getText().equals("X") && boton[2].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[0].getText().equals("O") && boton[1].getText().equals("O") && boton[2].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[3].getText().equals("X") && boton[4].getText().equals("X") && boton[5].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[3].getText().equals("O") && boton[4].getText().equals("O") && boton[5].getText().equals("O")) {
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[6].getText().equals("X") && boton[7].getText().equals("X") && boton[8].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[6].getText().equals("O") && boton[7].getText().equals("O") && boton[8].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[0].getText().equals("X") && boton[3].getText().equals("X") && boton[6].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")) {
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[1].getText().equals("X") && boton[4].getText().equals("X") && boton[7].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[1].getText().equals("O") && boton[4].getText().equals("O") && boton[7].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[2].getText().equals("X") && boton[5].getText().equals("X") && boton[8].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[2].getText().equals("O") && boton[5].getText().equals("O") && boton[8].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[0].getText().equals("X") && boton[4].getText().equals("X") && boton[8].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[0].getText().equals("O") && boton[4].getText().equals("O") && boton[8].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+			
+			if(boton[2].getText().equals("X") && boton[4].getText().equals("X") && boton[6].getText().equals("X")) {
+				JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
+				Prueba3 abrir = new Prueba3();
+				abrir.setVisible(true);
+			}else if(boton[2].getText().equals("O") && boton[4].getText().equals("O") && boton[6].getText().equals("O")){
+				Perder abrir = new Perder();
+				abrir.setVisible(true);
+			}
+		}else {
 			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
 			Prueba2 abrir = new Prueba2();
 			abrir.setVisible(true);
 		}
-		
-		if(boton[3].getText().equals("X") && boton[4].getText().equals("X") && boton[5].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[3].getText().equals("O") && boton[4].getText().equals("O") && boton[5].getText().equals("O")) {
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[6].getText().equals("X") && boton[7].getText().equals("X") && boton[8].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[6].getText().equals("O") && boton[7].getText().equals("O") && boton[8].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[0].getText().equals("X") && boton[3].getText().equals("X") && boton[6].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")) {
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[1].getText().equals("X") && boton[4].getText().equals("X") && boton[7].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[2].getText().equals("X") && boton[5].getText().equals("X") && boton[8].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[0].getText().equals("X") && boton[4].getText().equals("X") && boton[8].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		if(boton[2].getText().equals("X") && boton[4].getText().equals("X") && boton[6].getText().equals("X")) {
-			JOptionPane.showMessageDialog(null, "Has ganado, avanza a la siguiente prueba");
-			Prueba3 abrir = new Prueba3();
-			abrir.setVisible(true);
-		}else if(boton[0].getText().equals("O") && boton[3].getText().equals("O") && boton[6].getText().equals("O")){
-			Perder abrir = new Perder();
-			abrir.setVisible(true);
-		}else if(contador == 0) {
-			JOptionPane.showMessageDialog(null, "Has empatado, reinicia la prueba");
-			Prueba2 abrir = new Prueba2();
-			abrir.setVisible(true);
-		}
-		
-		
 	}
 }
